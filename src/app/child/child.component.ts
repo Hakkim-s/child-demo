4a9c3c9eddd616c2165f85e82769828da7a5fee2
krithikakl <jenifer.a@optisolbusiness.com>
2018-12-07 12:19:50 +0530

    get and set

---
+++ src/app/child/child.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css']
+})
+export class ChildComponent implements OnInit {
+
+  private _loggedIn: boolean;
+  message: string;
+
+  get loggedIn(): boolean {
+    return this._loggedIn;
+  }
+ @Input()
+   set loggedIn(value : boolean){
+     this._loggedIn= value;
+     if( value === true){
+       this.message = "welcome back Hakkim";
+     } else{
+       this.message = "please log in";
+     }
+     
+   }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+ 
