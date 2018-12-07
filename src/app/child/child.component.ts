62029ca1fd3867b272b41f993e15004db4803db1
krithikakl <jenifer.a@optisolbusiness.com>
2018-12-07 12:56:38 +0530

    ngonchanges

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,32 +1,39 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChange } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent implements OnInit {
+export class ChildComponent implements OnChanges {
 
-  private _loggedIn: boolean;
-  message: string;
+  @Input() loggedIn: boolean;
+    message: string;
 
-  get loggedIn(): boolean {
-    return this._loggedIn;
-  }
- @Input()
-   set loggedIn(value : boolean){
-     this._loggedIn= value;
-     if( value === true){
-       this.message = "welcome back Hakkim";
-     } else{
-       this.message = "please log in";
-     }
+//   get loggedIn(): boolean {
+//     return this._loggedIn;
+//   }
+//  @Input()
+//    set loggedIn(value : boolean){
+//      this._loggedIn= value;
+//      if( value === true){
+//        this.message = "welcome back Hakkim";
+//      } else{
+//        this.message = "please log in";
+//      }
      
-   }
+//    }
   constructor() { }
 
-  ngOnInit() {
+  ngOnChanges(changes : SimpleChange) {
+    console.log(changes);
+    const loggedInValue = changes['loggedIn'];
+    if(loggedInValue.currentValue === true){
+      this.message = ' Welcome back hakkim!!';
+    }else{
+      this.message = 'Please log in';
+    }
+    }
+    
   }
-
-}
  
